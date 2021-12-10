042979e8dbd5d97791f3da0fcb683deb98a543cb
kadiremrah <emrah.demic@gmail.com>
2021-12-10 10:07:43 +0300

    Update sort_domain.py

--- GetRootDomain/sort_domain.py
+++ GetRootDomain/sort_domain.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python 
 from fileinput import input
 for y in sorted([x.strip().split('.')[::-1] for x in input()]): print '.'.join(y[::-1])
-#usage sudo cat domains.txt | ./sort_domain.py > domains or python sort_domain.py dnsbl.conf.oisdfull > dnsbl.hosts
+# to sort your file  "sample.txt" on linux terminal command : python sort_domain.py sample.txt > output.txt
+# this will sort file lines in reverse order so you will get list of lines sorted by same root.domains
